1a85cc3a2217673a01119d72931df566a62f1f33
eltoMarqs <uelmcrz@gmail.com>
2022-07-08 18:29:03 -0300

    console

---
+++ freeCodeCamp/algoritmos-e-estruturas-de-dados-em-javascript/Depuracao/Depuração.js
@@ -0,0 +1,12 @@
+//Usar o console do JavaScript para verificar o valor de uma variável
+let a = 5;
+let b = 1;
+a++;
+// Altere apenas o código abaixo desta linha
+
+
+let sumAB = a + b;
+console.log(a);
+
+
+//
